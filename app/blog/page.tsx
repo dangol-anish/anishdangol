d5fa5441d08a41f686c5b5838406b85d3ca88b61
AD <154968000+dangol-anish@users.noreply.github.com>
2024-07-01 21:54:26 +0545

    added blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <>
+      <p>This page is currently under construction! </p>
+    </>
+  );
+}
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,11 +1,18 @@
 import { ModeToggle } from "../ModeToggle";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 export function Header() {
   return (
     <>
       <header id="top">
-        <div>
+        <div className="flex">
           <ModeToggle />
+          <Link href="/blog">
+            <Button variant="outline" size="icon">
+              B
+            </Button>
+          </Link>
         </div>
       </header>
     </>
