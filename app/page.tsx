c63e5ccef8e895ea0f4b78c1f05b70475db87ec4
AD <154968000+dangol-anish@users.noreply.github.com>
2024-09-24 17:58:43 +0545

    added graidents

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import GridSection from "@/components/GridSection";
 import HeroSection from "@/components/HeroSection";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
@@ -5,7 +6,7 @@ import { FaHome } from "react-icons/fa";
 export default function Home() {
   return (
     <>
-      <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <main className="relative bg-stone-950 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full">
           <FloatingNav
             navItems={[
@@ -17,6 +18,7 @@ export default function Home() {
             ]}
           />
           <HeroSection />
+          <GridSection />
         </div>
       </main>
     </>
