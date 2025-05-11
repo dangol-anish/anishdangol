3d6416d6e23f7fbf9fe8899d906c2a62fb8c03fb
Anish Dangol <dangol.anish001@gmail.com>
2025-05-11 09:53:26 +0545

    add: theme color

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,11 @@
-import ContactSection from "@/components/ContactSection";
-import FooterSection from "@/components/FooterSection";
-import GridSection from "@/components/GridSection";
-import HeroSection from "@/components/HeroSection";
-import { ProjectSection } from "@/components/ProjectSection";
-import SkillSection from "@/components/SkillSection";
-import { BackgroundBeams } from "@/components/ui/background-beams";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/custom/Theme";
 
 export default function Home() {
   return (
     <>
-      <main className="relative bg-stone-950 flex justify-center items-center flex-col overflow-hidden w-full ">
-        <div className=" w-full">
-          <FloatingNav />
-
-          <HeroSection />
-          <GridSection />
-          <ProjectSection />
-          <SkillSection />
-          <ContactSection />
-          <FooterSection />
-        </div>
-      </main>
+      <ModeToggle />
     </>
   );
 }
