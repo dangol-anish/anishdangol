3247b5ebd82d04494d5fd9f0e5df7f9ba23d2bd7
Anish Dangol <dangol.anish001@gmail.com>
2025-05-15 18:46:23 +0545

    update: added icon in blog

--- app/constants/blogItems.tsx
+++ app/constants/blogItems.tsx
@@ -4,14 +4,7 @@ export const blogItems = [
   {
     title: "Sphere",
     description: "Portfolio and blog built with astro.",
-    icon: <ArrowUpRight className="w-4 h-4" />,
+    live: "http://dangolanish.com.np",
     active: true,
-  },
-  {
-    title: "Nano",
-    description:
-      "Minimal portfolio and blog built with astro and no frameworks.",
-    icon: <ArrowUpRight className="w-4 h-4" />,
-    active: false,
   },
 ];
--- components/custom/body/SectionList.tsx
+++ components/custom/body/SectionList.tsx
@@ -74,7 +74,28 @@ export default function SectionList({
                     target="_blank"
                     className="text-sm hover:underline flex items-center gap-1"
                   >
-                    {item.icon}
+                    {item.icon ? (
+                      item.icon
+                    ) : (
+                      <button className="group not-prose inline-flex items-center gap-1 pl-2 pr-0.5 py-1 rounded-md font-bold bg-inherit  text-current hover:text-black hover:dark:text-white border-zinc-300 dark:border-zinc-700 hover:bg-zinc-200 hover:dark:bg-zinc-800 transition-colors duration-300 ease-in-out">
+                        <svg
+                          viewBox="0 0 24 24"
+                          className="size-5 stroke-[3px] fill-none stroke-current opacity-50 group-hover:opacity-100 transition-opacity duration-300 ease-in-out"
+                        >
+                          <line
+                            x1="5"
+                            y1="12"
+                            x2="19"
+                            y2="12"
+                            className="scale-x-0 translate-x-[10px] group-hover:translate-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"
+                          />
+                          <polyline
+                            points="12 5 19 12 12 19"
+                            className="-translate-x-2 group-hover:translate-x-0 transition-transform duration-300 ease-in-out"
+                          />
+                        </svg>
+                      </button>
+                    )}
                   </Link>
                 )}
               </div>
