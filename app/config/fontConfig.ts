3e0565ba4265e6c1d14695e0452a0bbf3db70acc
Anish Dangol <dangol.anish001@gmail.com>
2026-03-01 13:18:36 +0545

    website

--- app/config/fontConfig.ts
+++
@@ -1,6 +0,0 @@
-import { EB_Garamond } from "next/font/google";
-
-export const EbGaramond = EB_Garamond({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
--- app/constants/navItems.ts
+++
@@ -1,5 +0,0 @@
-export const navItems = [
-  { label: "projects", path: "/projects" },
-  { label: "blog", path: "/blog" },
-  { label: "contact", path: "#contact" },
-];
